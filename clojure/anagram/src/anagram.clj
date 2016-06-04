c99da38ececa6f7d630f8919cf3f2893254ac06e
rscnt <rascnt@gmail.com>
2016-06-03 18:04:24 -0600

    anangram
    Signed-off-by: rscnt <rascnt@gmail.com>

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,7 +1,23 @@
 (ns anagram
+  (:require [clojure.data :as data])
   (:require [clojure.string :as str]))
 
-(defn is-palindrome-of [word candidate] ())
+(defn count-letter-in-word [word letter]
+  (count (re-seq (re-pattern (str letter)) word)))
 
-(defn anagram-for [word list-candidates]
-  (filter (fn [candidate] (is-palindrome-of word candidate)) list-candidates))
+(defn build-hash-letter-count [word]
+  (let [letter-map {}]
+    (loop [letter word]
+      (update letter-map letter
+              (fn [oletter] (inc (get letter-map letter 0)))))))
+
+(defn is-palindrome-of [word candidate]
+  (cond
+    (= (count candidate) (count word)) (= (take 2 (data/diff (build-hash-letter-count word) (build-hash-letter-count candidate))))
+    :else false))
+
+(defn anagrams-for [word list-candidates]
+  (filter
+   (fn [candidate]
+     (is-palindrome-of word candidate))
+   (remove #(= (first %) word) list-candidates)))
--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -3,16 +3,6 @@
 
 (defn sub-ocurrences-in-string ([string sub] (count (re-seq (re-pattern (str "\\b" sub "\\b")) string))))
 
-;; A B C C C D A B
-;; -- {A 1}
-;; -- {A 1 B 1}
-;; -- {A 1 B 1 C 1}
-;; -- {A 1 B 1 C 2}
-;; -- {A 1 B 1 C 3}
-;; -- {A 1 B 1 C 3 D 1}
-;; -- {A 2 B 1 C 3 D 1}
-;; -- {A 2 B 2 C 3 D 1}
-
 (defn word-count
   ([text]
    (defn sub-counter [sub] (sub-ocurrences-in-string (str/lower-case text) (str/lower-case sub)))
